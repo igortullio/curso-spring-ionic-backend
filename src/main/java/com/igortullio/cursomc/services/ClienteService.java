32ea66878f526702ebedde0449ae7b257aa1fb99
Igor Túllio <igor.goncalves@basis.com.br>
2018-07-04 10:56:51 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/igortullio/cursomc/services/ClienteService.java
+++ src/main/java/com/igortullio/cursomc/services/ClienteService.java
@@ -14,6 +14,7 @@ import com.igortullio.cursomc.services.exceptions.AuthorizationException;
 import com.igortullio.cursomc.services.exceptions.DataIntegrityException;
 import com.igortullio.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,6 +24,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -41,6 +43,12 @@ public class ClienteService {
 
     @Autowired
     private S3Service s3Service;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Value("${img.prefix.client.profile}")
+    private String prefix;
 
     public Cliente find(Integer id){
 
@@ -115,13 +123,10 @@ public class ClienteService {
         if (userSS == null){
             throw new AuthorizationException("Acesso Negado");
         }
-        URI uri = s3Service.uploadFile(multipartFile);
 
-        Cliente cliente = find(userSS.getId());
-        cliente.setImageUrl(uri.toString());
-        clienteRepository.save(cliente);
-
-        return uri;
+        BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+        String fileName = prefix + userSS.getId() + ".jpg";
+        return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
     }
 
 }
